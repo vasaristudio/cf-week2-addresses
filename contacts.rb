6d6a1390063a859e9804de9e2cf8f217aa324b16
Derik Strattan <derik.strattan@gmail.com>
2013-08-21 14:21:31 -0700

    Added age to the person object, and created a column heading 12 spaces rjustified.

--- contacts.rb
+++ contacts.rb
@@ -4,11 +4,14 @@ class Person
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :phone
+  attr_accessor :age
 
   def full_name
     first_name + " " + last_name
   end
 end
+
+header = ["Name", "Phone", "Age"]
 
 class Contacts
   def self.run
@@ -18,10 +21,10 @@ class Contacts
       command = ask("> ")
 
       if command == "list"
-        puts "Name".ljust(30) + "Phone".rjust(12)
-        puts "-" * 42
+        puts "Name".ljust(30) + "Phone".rjust(12) + "Age".rjust(12)
+        puts "-" * 54
         people.each do |person|
-          puts person.full_name.ljust(30) + person.phone.rjust(12)
+          puts person.full_name.ljust(30) + person.phone.rjust(12) + person.age.rjust(12)
         end
 
       elsif command == "add"
@@ -30,6 +33,7 @@ class Contacts
         person.first_name = ask("First Name: ")
         person.last_name  = ask("Last Name: ")
         person.phone      = ask("Phone: ")
+        person.age        = ask("Age: ")
 
         people.push(person)
       elsif command == "quit"
@@ -40,3 +44,4 @@ class Contacts
 end
 
 Contacts.run
+
